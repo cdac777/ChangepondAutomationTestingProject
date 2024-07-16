da29d33d4b2ffbdbbd1266d57c90213ce4b5376b
Dharanesh2612 <rdharanesh26122000@gmail.com>
2024-07-16 19:31:27 +0530

    updated

--- Quiz-App (Ajith , Vijay , Prasanth , Dharanesh )/POMTestDesign/src/test/java/PageObjects/VoucherCrudTest.java
+++ Quiz-App (Ajith , Vijay , Prasanth , Dharanesh )/POMTestDesign/src/test/java/PageObjects/VoucherCrudTest.java
@@ -12,7 +12,7 @@ public class VoucherCrudTest {
     By add_btn = By.xpath("//a[@class='btn btn-primary mb-2']");
     By ent_voucher = By.xpath("//input[@name='voucher_number']");
     By submit = By.xpath("//button[@type='submit']");
-    By delete = By.xpath("//tbody/tr[4]/td[3]/button[1]//*[name()='svg']");
+//    By delete = By.xpath("//tbody/tr[4]/td[3]/button[1]//*[name()='svg']//*[name()='path' and contains(@d,'M6 19c0 1.')]");
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -33,11 +33,11 @@ public class VoucherCrudTest {
         Alert alert = driver.switchTo().alert();
         alert.accept();
 
-        driver.findElement(delete).click();
-
-        // Wait for alert to be present and then accept it
-        wait.until(ExpectedConditions.alertIsPresent());
-        alert = driver.switchTo().alert();
-        alert.accept();
+//        driver.findElement(delete).click();
+//
+//        // Wait for alert to be present and then accept it
+//        wait.until(ExpectedConditions.alertIsPresent());
+//        alert = driver.switchTo().alert();
+//        alert.accept();
     }
 }
